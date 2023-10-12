3d83dd7127e14689273c4bf1b835af0aa883b391
sirinekabboura <kaboura.sirine@gmail.com>
2023-10-12 17:48:31 +0100

    testUnitaire

--- src/main/java/tn/esprit/spring/kaddem/services/ContratServiceImpl.java
+++ src/main/java/tn/esprit/spring/kaddem/services/ContratServiceImpl.java
@@ -1,6 +1,7 @@
 package tn.esprit.spring.kaddem.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
 import lombok.extern.slf4j.Slf4j;
---
+++ src/test/java/tn/esprit/spring/kaddem/ContratServiceImplTest.java
@@ -0,0 +1,45 @@
+package tn.esprit.spring.kaddem;
+
+
+import org.junit.jupiter.api.*;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import tn.esprit.spring.kaddem.entities.Contrat;
+import tn.esprit.spring.kaddem.services.IContratService;
+
+import java.text.ParseException;
+import java.util.Date;
+import java.util.List;
+
+import static tn.esprit.spring.kaddem.entities.Specialite.CLOUD;
+
+
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+@SpringBootTest
+public class ContratServiceImplTest {
+    @Autowired
+    IContratService contratService;
+
+    @Test
+    @Order(1)
+    public void TestRetrieveAllContrats()
+    {
+        List<Contrat> contrats = contratService.retrieveAllContrats();
+        Assertions.assertEquals(1,contrats.size());
+    }
+
+    @Test
+    @Order(2)
+    public void TestAddContract() throws ParseException {
+        Date d1 = new Date();
+        Date d2 = new Date();
+        Contrat contrat=new Contrat(d1,d2,CLOUD,true,5000);
+        Contrat contrat1=contratService.addContrat(contrat);
+        Assertions.assertEquals(contrat.getArchive(),contrat1.getArchive());
+    }
+
+
+
+
+}
