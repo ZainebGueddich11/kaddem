e263e88f366a753ff0c035de3c72292fa9f27dcc
hamdyys <hamdi.selmi@esprit.tn>
2023-10-12 17:49:37 +0100

    testtest

---
+++ src/main/java/tn/esprit/spring/kaddem/services/services/IEquipeService.java
@@ -0,0 +1,14 @@
+package tn.esprit.spring.kaddem.services.services;
+
+import tn.esprit.spring.kaddem.services.entities.Equipe;
+
+import java.util.List;
+
+public interface IEquipeService {
+    public List<Equipe> retrieveAllEquipes();
+    public Equipe addEquipe(Equipe e);
+    public  void deleteEquipe(Integer idEquipe);
+    public Equipe updateEquipe(Equipe e);
+    public Equipe retrieveEquipe(Integer equipeId);
+    public void evoluerEquipes();
+}
---
+++ src/test/java/tn/esprit/spring/kaddem/services/EtudiantServicesImplTest.java
@@ -0,0 +1,42 @@
+package tn.esprit.spring.kaddem.services;
+
+
+import org.junit.jupiter.api.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import tn.esprit.spring.kaddem.services.entities.Etudiant;
+import tn.esprit.spring.kaddem.services.entities.Option;
+import tn.esprit.spring.kaddem.services.services.IEtudiantService;
+
+
+import java.util.List;
+
+import static tn.esprit.spring.kaddem.services.entities.Option.GAMIX;
+
+
+@SpringBootTest
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+public class EtudiantServicesImplTest {
+
+    @Autowired
+    IEtudiantService EtudiantServiceImpl;
+
+    @Test
+    @Order(1)
+    public void TestRetrieveAllEtudiants()
+    {
+        List<Etudiant> contrats = EtudiantServiceImpl.retrieveAllEtudiants();
+        Assertions.assertEquals(1,contrats.size());
+    }
+
+    @Test
+    @Order(2)
+    public void testAddEtudiant()
+    {
+        Etudiant etudiant=new Etudiant(1,"Kabboura","" ,GAMIX);
+        Etudiant etudiant1= EtudiantServiceImpl.addEtudiant(etudiant);
+        Assertions.assertEquals(etudiant.getNomE(),etudiant1.getNomE());
+    }
+
+
+}
