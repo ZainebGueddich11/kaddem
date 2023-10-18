999c38e3e24e8905a8d8a04ea12ae77656981833
ZainebGueddich11 <zainebgueddich28@gmail.com>
2023-10-18 14:34:47 +0100

    update equipetestMock

--- src/test/java/tn/esprit/spring/kaddem/EquipeServiceImplMock.java
+++ src/test/java/tn/esprit/spring/kaddem/EquipeServiceImplMock.java
@@ -7,6 +7,7 @@ import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import tn.esprit.spring.kaddem.entities.Equipe;
 import tn.esprit.spring.kaddem.entities.Niveau;
 import tn.esprit.spring.kaddem.repositories.EquipeRepository;
@@ -21,6 +22,7 @@ import static org.mockito.Mockito.when;
 @SpringBootTest
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 @ExtendWith(MockitoExtension.class)
+@ActiveProfiles("test")
 public class EquipeServiceImplMock {
     @Mock
     EquipeRepository eqRep;
