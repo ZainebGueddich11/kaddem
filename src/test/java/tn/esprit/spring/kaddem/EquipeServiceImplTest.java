abfbf70fa22443b775654209711e2b9be84cf464
ZainebGueddich11 <zainebgueddich28@gmail.com>
2023-10-18 13:10:13 +0100

    add mock and updates equipeTest

--- src/test/java/tn/esprit/spring/kaddem/EquipeServiceImplTest.java
+++ src/test/java/tn/esprit/spring/kaddem/EquipeServiceImplTest.java
@@ -1,7 +1,6 @@
 package tn.esprit.spring.kaddem;
 
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import tn.esprit.spring.kaddem.entities.Equipe;
@@ -11,16 +10,19 @@ import tn.esprit.spring.kaddem.services.IEquipeService;
 import java.util.List;
 
 @SpringBootTest
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class EquipeServiceImplTest {
     @Autowired
     IEquipeService equipeService;
     @Test
+    @Order(1)
     public void TestRetrieveAllEquipe()
     {
         List<Equipe> equipes = equipeService.retrieveAllEquipes();
         Assertions.assertEquals(1,equipes.size());
     }
     @Test
+    @Order(2)
     public void TestaddEquipe()
     {
        String nomEquipe= "equipe 1";
@@ -28,5 +30,11 @@ public class EquipeServiceImplTest {
       Equipe eqAddded = equipeService.addEquipe(eq);
       Assertions.assertEquals(eq.getNomEquipe(),eq.getNiveau());
     }
-
+    @Test
+    @Order(3)
+    public void testDeleteUser()
+    {
+        equipeService.deleteEquipe(1);
+        Assertions.assertNull(equipeService.retrieveEquipe(1));
+    }
 }
