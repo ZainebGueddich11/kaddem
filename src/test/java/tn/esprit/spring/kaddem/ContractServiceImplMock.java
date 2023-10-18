1b02f9fab49b8c4cdaa24b21687cf2c53f76bbd5
sirinekabboura <kaboura.sirine@gmail.com>
2023-10-18 14:31:32 +0100

    mockkk4

--- src/test/java/tn/esprit/spring/kaddem/ContractServiceImplMock.java
+++ src/test/java/tn/esprit/spring/kaddem/ContractServiceImplMock.java
@@ -17,7 +17,6 @@ import tn.esprit.spring.kaddem.services.ContratServiceImpl;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
-import java.util.Optional;
 
 import static tn.esprit.spring.kaddem.entities.Specialite.CLOUD;
 
@@ -32,27 +31,22 @@ public class ContractServiceImplMock {
     @InjectMocks
     ContratServiceImpl contratService;
 
-
-    /*Contrat contrat2=new Contrat(new Date(),new Date(),CLOUD,false,5600);
-    Contrat contrat3=new Contrat(new Date(),new Date(),CLOUD,true,5700);
-    List<Contrat> contratList = new ArrayList<Contrat>()
-    {
-
-
-        @Override
-        public void add(int index, Contrat element) {
-            super.add(index, contrat2);
-            super.add(index, contrat3);
-        }
-    };
-*/
     Contrat contrat2 = new Contrat(1,new Date(),new Date(),CLOUD,true,5000);
+
 
     @Test
     public void TestRetrieveAllContrats()
     {
-        Mockito.when(contratRepository.findByIdContrat(Mockito.anyInt())).thenReturn(contrat2);
-        Contrat contrat = contratService.retrieveContrat(1);
-        Assertions.assertNotNull(contrat);
+        List<Contrat> contratsFactices = new ArrayList<>();
+        Mockito.when(contratService.retrieveAllContrats()).thenReturn(contratsFactices);
+
+        // Appelez la méthode que vous testez
+        List<Contrat> result = contratService.retrieveAllContrats();
+
+        // Vérifiez les assertions pour vous assurer que le comportement est conforme à vos attentes
+        // Exemple : vérifiez la taille de la liste
+        Assertions.assertEquals(0, result.size());
+
+
     }
 }
