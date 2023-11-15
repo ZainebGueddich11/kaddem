12fecf4192a2c23c65fe61c0a345599904da4f71
hamdyys <hamdi.selmi@esprit.tn>
2023-11-15 16:34:45 +0100

    testststst

--- src/test/java/tn/esprit/spring/kaddem/services/EtudiantServiceImplMockTest.java
+++ src/test/java/tn/esprit/spring/kaddem/services/EtudiantServiceImplMockTest.java
@@ -1,30 +1,37 @@
 package tn.esprit.spring.kaddem.services;
+
+
+import org.junit.Test;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.MethodOrderer;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.TestMethodOrder;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import tn.esprit.spring.kaddem.services.entities.Etudiant;
 import tn.esprit.spring.kaddem.services.repositories.EtudiantRepository;
 import tn.esprit.spring.kaddem.services.services.EtudiantServiceImpl;
 
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 import static tn.esprit.spring.kaddem.services.entities.Option.GAMIX;
+
 
 @SpringBootTest
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 @ExtendWith(MockitoExtension.class)
-public class EtudiantServiceImplMock {
+@RunWith(MockitoJUnitRunner.class)
+public class EtudiantServiceImplMockTest {
 
 
 
@@ -37,23 +44,11 @@ public class EtudiantServiceImplMock {
         EtudiantServiceImpl etudiantService;
 
 
-        /*Contrat contrat2=new Contrat(new Date(),new Date(),CLOUD,false,5600);
-        Contrat contrat3=new Contrat(new Date(),new Date(),CLOUD,true,5700);
-        List<Contrat> contratList = new ArrayList<Contrat>()
-        {
 
 
-            @Override
-            public void add(int index, Contrat element) {
-                super.add(index, contrat2);
-                super.add(index, contrat3);
-            }
-        };
-    */
         Etudiant etudiant=new Etudiant(3,"Kabboura","Sirine" ,GAMIX);
-
-        @Test
-        public void TestRetrieveAllContrats()
+@Test
+        public void TestRetrieveAllEtudiants()
         {
             when(etudiantRepository.findById(1)).thenReturn(Optional.of(etudiant));
             Etudiant etudiant1=etudiantService.retrieveEtudiant(1);
